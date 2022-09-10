e29352ce4dba6b693c7a794c5981f47a623914a1
LoliweLoliwe <109585257+LoliweLoliwe@users.noreply.github.com>
2022-09-10 22:55:05 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+
 const cardList = [
   {
       title: "Mjita 2",
@@ -12,6 +13,7 @@ const cardList = [
       desciption: "Demo desciption of a man in a suit"
   }
 ]
+
 const clickMe = () => {
   alert("Thanks for clicking me. Hope you have a nice day!")
 }
@@ -24,16 +26,28 @@ const addCards = (items) => {
   '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
   '<div class="card-reveal">'+
       '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-      '<p class="card-text">'+item.desciption+'</p>'+
+      '<p class="card-text card-desc-color">'+item.desciption+'</p>'+
     '</div></div></div>';
     $("#card-section").append(itemToAppend)
   });
 }
 
+const submitForm = () => {
+  let formData = {};
+  formData.first_name = $('#first_name').val();
+  formData.last_name = $('#last_name').val();
+  formData.password = $('#password').val();
+  formData.email = $('#email').val();
+
+  console.log("Form Data Submitted: ", formData);
+}
+
 $(document).ready(function(){
   $('.materialboxed').materialbox();
   $('#clickMeButton').click(()=>{
-      clickMe();
+    clickMe();
   })
   addCards(cardList);
-});
+})
+
+
